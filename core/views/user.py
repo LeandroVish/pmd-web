16a36dee7308aacbb0320446d2f7ebf7b23cc566
LeandroVish <leandrogustavosantos.info@gmail.com>
2024-03-08 09:33:15 -0300

    up

--- core/views/user.py
+++ core/views/user.py
@@ -2,8 +2,11 @@ from rest_framework.viewsets import ModelViewSet
 
 from core.models import User
 from core.serializers import UserSerializer
+from core.views import CategoriaViewSet, EditoraViewSet
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    
